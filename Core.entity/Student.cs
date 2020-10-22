9fc1ecab566c78a073c569ab70eb603269f2a27c
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2020-10-22 11:27:36 +0200

    Studentupdate

---
+++ Core.entity/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.entity
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public string PostalDistrict { get; set; }
+        public string Email { get; set; }
+
+    }
+}
