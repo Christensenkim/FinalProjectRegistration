ce3bbef97fa68b0ad2840ed45cab570b613c5004
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2020-10-22 11:56:07 +0200

    Update StudentService.cs

--- Core/ApplicationService/Services/StudentService.cs
+++ Core/ApplicationService/Services/StudentService.cs
@@ -1,7 +1,36 @@
-﻿namespace Core.ApplicationService.Services
+﻿using System.Collections.Generic;
+using Core.DomainService;
+using Core.entity;
+
+namespace Core.ApplicationService.Services
 {
-    public class StudentService
+    public class StudentService: IStudentService
     {
-        
+        private IStudentRepository _studentRepository;
+
+        public IEnumerable<Student> GetAll()
+        {
+            return _studentRepository.GetAll();
+        }
+
+        public Student GetById(int id)
+        {
+            return _studentRepository.GetById(id);
+        }
+
+        public void Add(Student s)
+        {
+            _studentRepository.Add(s);
+        }
+
+        public void Update(Student s)
+        {
+            _studentRepository.Update(s);
+        }
+
+        public void Remove(Student s)
+        {
+            _studentRepository.Remove(s);
+        }
     }
 }
