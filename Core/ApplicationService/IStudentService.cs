dabae7a5aa9351bcfbb131487a8eea4d912ffa61
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2020-10-22 12:03:44 +0200

    bugfix

--- Core/ApplicationService/IStudentService.cs
+++ Core/ApplicationService/IStudentService.cs
@@ -5,28 +5,10 @@ namespace Core.ApplicationService
 {
     public interface IStudentService
     {
-        public void Add(Student s)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IEnumerable<Student> GetAll()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Student GetById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Remove(Student s)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Update(Student s)
-        {
-            throw new System.NotImplementedException();
-        }
+        IEnumerable<Student> GetAll();
+        Student GetById(int id);
+        void Add(Student s);
+        void Update(Student s);
+        void Remove(Student s);
+    }
 }
