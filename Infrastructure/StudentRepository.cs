a875c51e2834ae7819e4658ce9d6f488cd8bf0ef
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2020-10-22 11:49:18 +0200

    Yep

--- Infrastructure/StudentRepository.cs
+++ Infrastructure/StudentRepository.cs
@@ -1,7 +1,34 @@
-﻿namespace Infrastructure
+﻿using Core.DomainService;
+using Core.entity;
+using System.Collections.Generic;
+
+namespace Infrastructure
 {
-    public class StudentRepository: IStudentRepository
+    public class StudentRepository : IStudentRepository
     {
-        
+        public void Add(Student s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Student GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(Student s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Student s)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
