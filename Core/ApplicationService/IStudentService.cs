c47804edfcc1eefe1d5965f98aec98b5a114c10f
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2020-10-22 12:01:19 +0200

    Update IStudentService.cs

--- Core/ApplicationService/IStudentService.cs
+++ Core/ApplicationService/IStudentService.cs
@@ -1,7 +1,32 @@
-﻿namespace Core.ApplicationService
+﻿using System.Collections.Generic;
+using Core.entity;
+
+namespace Core.ApplicationService
 {
     public interface IStudentService
     {
-        
-    }
+        public void Add(Student s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Student GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(Student s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Student s)
+        {
+            throw new System.NotImplementedException();
+        }
 }
