958d99dd910683d0bbe81245d8701f284e84628b
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2020-10-29 10:03:34 +0100

    Entity update

--- Core.entity/Team.cs
+++ Core.entity/Team.cs
@@ -1,7 +1,10 @@
-﻿namespace Core.entity
+﻿using System.Collections.Generic;
+
+namespace Core.entity
 {
     public class Team
     {
-        
+        public int id { get; set; }
+        public List<Student> TeamList { get; set; }
     }
 }
